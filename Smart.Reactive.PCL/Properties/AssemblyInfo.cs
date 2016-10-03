411f3b2a9415867fefbba025c7c8cdb32a2bac90
usausa <machi.pon@gmail.com>
2016-10-03 14:40:48 +0900

    Add NotifyPropertyChangedExtensions

--- Smart.Reactive.PCL/Properties/AssemblyInfo.cs
+++ Smart.Reactive.PCL/Properties/AssemblyInfo.cs
@@ -1,20 +1,24 @@
-﻿using System.Resources;
+﻿using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("Smart.Reactive.PCL")]
+[assembly: AssemblyTitle("Smart.Reactive")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Smart.Reactive.PCL")]
+[assembly: AssemblyProduct("Smart.Reactive")]
 [assembly: AssemblyCopyright("Copyright (C)  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("ja")]
+[assembly: NeutralResourcesLanguage("")]
+
+[assembly: CLSCompliant(true)]
+
+[assembly: ComVisible(false)]
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
@@ -23,8 +27,8 @@ using System.Runtime.InteropServices;
 //      ビルド番号
 //      リビジョン
 //
-// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ Smart.Reactive/Reactive/NotifyPropertyChangedExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Smart.Reactive.Reactive
+{
+    using System;
+    using System.ComponentModel;
+    using System.Reactive.Linq;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public static class NotifyPropertyChangedExtensions
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IObservable<PropertyChangedEventArgs> AsObservable(
+            this INotifyPropertyChanged source)
+        {
+            return Observable.FromEvent<PropertyChangedEventHandler, PropertyChangedEventArgs>(
+                h => (s, e) => h(e),
+                h => source.PropertyChanged += h,
+                h => source.PropertyChanged -= h);
+        }
+    }
+}
