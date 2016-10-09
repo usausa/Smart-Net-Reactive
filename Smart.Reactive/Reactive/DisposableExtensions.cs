05874e54b10be8656cadf9617fdc7f56edb0cccd
usausa <machi.pon@gmail.com>
2016-10-09 14:42:10 +0900

    Add Extensions

---
+++ Smart.Reactive/Reactive/DisposableExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Smart.Reactive.Reactive
+{
+    using System;
+    using System.Reactive.Disposables;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public static class DisposableExtensions
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="disposable"></param>
+        /// <param name="compositeDisposable"></param>
+        public static void AddDisposableTo(this IDisposable disposable, CompositeDisposable compositeDisposable)
+        {
+            if (compositeDisposable == null)
+            {
+                throw new ArgumentNullException(nameof(compositeDisposable));
+            }
+
+            compositeDisposable.Add(disposable);
+        }
+    }
+}
---
+++ Smart.Reactive/Reactive/ObservableExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Smart.Reactive.Reactive
+{
+    using System;
+    using Smart.ComponentModel;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public static class ObservableExtensions
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="observable"></param>
+        /// <param name="holder"></param>
+        /// <returns></returns>
+        public static IDisposable Bind<T>(this IObservable<T> observable, IValueHolder<T> holder)
+        {
+            return observable.Subscribe(_ => holder.Value = _);
+        }
+    }
+}
