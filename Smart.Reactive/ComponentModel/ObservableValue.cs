7bb4a187f0148174863910aea931cb5e8e3a2b66
usausa <machi.pon@gmail.com>
2016-11-02 10:38:29 +0900

    Add ObservableValue

---
+++ Smart.Reactive/ComponentModel/ObservableValue.cs
@@ -0,0 +1,63 @@
+﻿namespace Smart.ComponentModel
+{
+    using System;
+    using System.Reactive.Subjects;
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class ObservableValue<T> : IValueHolder<T>, IObservable<T>, IDisposable
+    {
+        private readonly Subject<T> subject = new Subject<T>();
+
+        private T lastValue;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public T Value
+        {
+            get
+            {
+                return lastValue;
+            }
+            set
+            {
+                subject.OnNext(value);
+            }
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public ObservableValue()
+        {
+            subject.Subscribe(x => lastValue = x);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public void Dispose()
+        {
+            if (subject.IsDisposed)
+            {
+                return;
+            }
+
+            subject.OnCompleted();
+            subject.Dispose();
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="observer"></param>
+        /// <returns></returns>
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            return subject.Subscribe(observer);
+        }
+    }
+}
