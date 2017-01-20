b06b97a31f87ed65f1cd83674b39508ab8925d8f
usausa <machi.pon@gmail.com>
2017-01-20 11:56:50 +0900

    Update ObserbavleExtensions

--- Smart.Reactive/ComponentModel/ObservableExtensions.cs
+++ Smart.Reactive/ComponentModel/ObservableExtensions.cs
@@ -14,9 +14,28 @@
         /// <param name="observable"></param>
         /// <param name="holder"></param>
         /// <returns></returns>
-        public static IDisposable SubscribeValue<T>(this IObservable<T> observable, IValueHolder<T> holder)
+        public static IDisposable SubscribeValue<T>(
+            this IObservable<T> observable,
+            IValueHolder<T> holder)
         {
             return observable.Subscribe(x => holder.Value = x);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="observable"></param>
+        /// <param name="selector"></param>
+        /// <param name="holder"></param>
+        /// <returns></returns>
+        public static IDisposable SubscribeValue<TSource, TResult>(
+            this IObservable<TSource> observable,
+            Func<TSource, TResult> selector,
+            IValueHolder<TResult> holder)
+        {
+            return observable.Subscribe(x => holder.Value = selector(x));
         }
     }
 }
