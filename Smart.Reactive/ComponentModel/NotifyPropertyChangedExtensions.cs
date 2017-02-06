702efd54d447dd902049bf7e5de8db20caab0b25
usausa <machi.pon@gmail.com>
2017-02-06 09:08:55 +0900

    Update NotifyPropertyChangedExtensions for NotificationValue

--- Smart.Reactive/ComponentModel/NotifyPropertyChangedExtensions.cs
+++ Smart.Reactive/ComponentModel/NotifyPropertyChangedExtensions.cs
@@ -40,5 +40,22 @@
                 .Where(x => x.PropertyName == propertyName)
                 .Select(x => source);
         }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IObservable<T> AsObservable<T>(
+            this NotificationValue<T> source)
+        {
+            return Observable.FromEvent<PropertyChangedEventHandler, PropertyChangedEventArgs>(
+                    h => (s, e) => h(e),
+                    h => source.PropertyChanged += h,
+                    h => source.PropertyChanged -= h)
+                .Where(x => x.PropertyName == nameof(NotificationValue<T>.Value))
+                .Select(x => source.Value);
+        }
     }
 }
