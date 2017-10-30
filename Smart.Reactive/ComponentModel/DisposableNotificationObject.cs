10d936a850b88a492b2f42dac8cd33d1eebba7a1
usausa <machi.pon@gmail.com>
2017-10-30 17:56:36 +0900

    Add DisposableNotificationObject

---
+++ Smart.Reactive/ComponentModel/DisposableNotificationObject.cs
@@ -0,0 +1,45 @@
+﻿namespace Smart.ComponentModel
+{
+    using System;
+    using System.Reactive.Disposables;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public abstract class DisposableNotificationObject : NotificationObject, IDisposable
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        protected CompositeDisposable Disposables { get; } = new CompositeDisposable();
+
+        /// <summary>
+        ///
+        /// </summary>
+        ~DisposableNotificationObject()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Disposables.Dispose();
+            }
+        }
+    }
+}
