f76ae2c9621d98f8b86762a88617f716d5f8493b
usausa <machi.pon@gmail.com>
2021-05-21 19:37:29 +0900

    Update extensions

--- Smart.Reactive/Reactive/DisposableExtensions.cs
+++ Smart.Reactive/Reactive/DisposableExtensions.cs
@@ -1,15 +1,15 @@
 namespace Smart.Reactive
 {
     using System;
-    using System.Collections.Generic;
+    using System.Reactive.Disposables;
 
     public static class DisposableExtensions
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Ignore")]
-        public static T AddTo<T>(this T disposable, ICollection<IDisposable> collection)
+        public static T DisposableBy<T>(this T disposable, CompositeDisposable disposables)
             where T : IDisposable
         {
-            collection.Add(disposable);
+            disposables.Add(disposable);
             return disposable;
         }
     }
